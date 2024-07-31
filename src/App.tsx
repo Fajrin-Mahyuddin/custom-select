b5c1bb745737f64abba9501225c576395020f37f
fajrin <fajrin.telkom12@gmail.com>
2024-08-01 01:40:40 +0800

    fix prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
-import SelectDropdown, { OptionMenuList, OptionPropsList, components } from '@fajrindev/custom-select'
+import
+SelectDropdown,
+{
+  // OptionMenuList, OptionPropsList, components
+} from '@fajrindev/custom-select'
 
 const stateOptions = [
   { value: "Strawberry", label: "Strawberry" },
@@ -16,7 +20,7 @@ function App() {
     setValue([]);
   }, [toggle]);
   return (
-    <div className="max-w-[800px] m-auto mt-[100px] h-[100px] bg-slate-500">
+    <div className="max-w-[800px] m-auto mt-[100px]">
       <button
         onClick={() => setToggle((e) => ({ ...e, withSearch: !e.withSearch }))}
         className="bg-[#975FCF] mb-2 mr-2 rounded-md px-6 text-[white] py-2"
@@ -36,59 +40,59 @@ function App() {
         withSearch={toggle.withSearch}
         options={stateOptions}
         isMulti={toggle.isMulti}
-        className='w-[400px] bg-red-400'
+        // className='w-[400px] bg-red-400'
         value={value}
-        style={{
-          option: () => ({
-            backgroundColor: "salmon",
-            "&:hover": {
-              backgroundColor: "green"
-            }
-          })
-        }}
+        // style={{
+        //   option: () => ({
+        //     backgroundColor: "salmon",
+        //     "&:hover": {
+        //       backgroundColor: "green"
+        //     }
+        //   })
+        // }}
         onChange={(e) => {
           setValue(e as typeof value);
           console.log("value", e);
         }}
         name="name"
-        optionList={Menu}
+      // optionList={Menu}
       />
     </div>
   )
 }
 
-const Menu = ((props: OptionPropsList) => {
-  return (
-    <div className="hover:bg-red-400">
-      <components.Option {...props}>
-        <span className='flex flex-row items-center gap-2 justify-start'>
-          <RemoveIcon />{props.children}
-        </span>
-      </components.Option>
-    </div>
-  );
-}) as OptionMenuList;
+// const Menu = ((props: OptionPropsList) => {
+//   return (
+//     <div className="hover:bg-red-400">
+//       <components.Option {...props}>
+//         <span className='flex flex-row items-center gap-2 justify-start'>
+//           <RemoveIcon />{props.children}
+//         </span>
+//       </components.Option>
+//     </div>
+//   );
+// }) as OptionMenuList;
 
-const RemoveIcon = () => {
-  return (
-    <svg viewBox="0 0 512 512" fill="currentColor" height="1em" width="1em">
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeMiterlimit={10}
-        strokeWidth={32}
-        d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
-      />
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={32}
-        d="M320 320L192 192M192 320l128-128"
-      />
-    </svg>
-  );
-};
+// const RemoveIcon = () => {
+//   return (
+//     <svg viewBox="0 0 512 512" fill="currentColor" height="1em" width="1em">
+//       <path
+//         fill="none"
+//         stroke="currentColor"
+//         strokeMiterlimit={10}
+//         strokeWidth={32}
+//         d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+//       />
+//       <path
+//         fill="none"
+//         stroke="currentColor"
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         strokeWidth={32}
+//         d="M320 320L192 192M192 320l128-128"
+//       />
+//     </svg>
+//   );
+// };
 
 export default App
